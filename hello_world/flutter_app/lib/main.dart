ce9addfa90840cdd74e51801edbe77d3649f4771
Haraka Risasi <harakarisasi@gmail.com>
2021-05-15 04:08:10 +0700

    - pubspec.yaml - подключил Google Fonts. - Изменил шрифт в приложении. - Понял разницу HotReload и HotRestart. - Понял для чего нужен StateLess вижджет. - Более подробно разобрался с Древом виджетов. - Инстанцировал в метод Main() мой основной виджет. - Разобрался, что значит @override. - Widget Build. - Трансформировал метод main в однострочный с помощью =>.

--- hello_world/flutter_app/lib/main.dart
+++ hello_world/flutter_app/lib/main.dart
@@ -1,37 +1,54 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(HarakaTestApp());
+// runApp делает переданный ей виджет, корневым виджетом дерева.
+// Поскольку MaterialApp - это высокоуровневый виджет, то передаем его непосредственно в ф-ю runApp().
+
+class HarakaTestApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       title: "HarakaTestApp",
-      home: Scaffold(
-        // Реализует базовую структуру визуального макета материального дизайна.
-        appBar: AppBar(
-          title: Text("Flutter tutorial"),
-          centerTitle: true,
-          backgroundColor: Colors.black38,
-        ),
-        body: Text(
-          "Hello World!!!",
-          style: TextStyle(
-              fontSize: 14.00,
-              fontStyle: FontStyle.italic,
-              fontWeight: FontWeight.w300,
-              decoration: TextDecoration.lineThrough,
-              letterSpacing: 3.89,
-              // color: Color(0xFFA94AF3) // If I want to use the HEX color code.
-              color: Colors.blue[300]),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Text("ADD"),
-        ),
-        backgroundColor: Color(0x612FDFEE),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-    ),
-  );
-  //runApp делает переданный ей виджет, корневым виджетом дерева.
-  // Поскольку MaterialApp - это высокоуровневый виджет, то передаем его непосредственно в ф-ю runApp().
-  // home - это виджет (в данном случае - аргумент), который отобразит все, что будет на экране при старте.
+      // home - это виджет, который отобразит все, что будет на экране при старте.
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // Реализует базовую структуру визуального макета материального дизайна.
+      appBar: AppBar(
+        title: Text("Flutter tutorial", style: GoogleFonts.raleway()),
+        centerTitle: true,
+        backgroundColor: Colors.black38,
+      ),
+      body: Text(
+        "Hello World!!",
+        style: GoogleFonts.lobster(
+          textStyle: TextStyle(
+            fontSize: 30.00,
+//            color: Color(0xFFA94AF3) // If I want to use the HEX color code.
+            color: Colors.yellowAccent,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text("ADD"),
+      ),
+      backgroundColor: Color(0x612FDFEE),
+    );
+  }
 }
