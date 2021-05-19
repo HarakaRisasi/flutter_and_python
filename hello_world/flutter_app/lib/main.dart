91b4a5ecc236b508126ce57139827403d0c6baa4
Haraka Risasi <harakarisasi@gmail.com>
2021-05-20 05:48:49 +0700

    Разбрался с тем:
    - Как не используя виджеты Column и Row создать неплохой интерфейс (например: обычно - то что пользователи должны видеть в каждом элементе списка.(заголовок, немного информации, какую-то иконку))
    - Виджет ListStyle - помогает создать виджет списка.
    - Как добавлять Текст.
    - Как добавлять Иконки.
    - Как деактивировать "Нажатие".
    - Как выделить контент внутри плитки (можно комбинировать вместе с Длинным нажатием(для выделения текста).)
    - Как весить CallBack на виджет.
    - CallBack - на короткое нажатие.
    - CallBack - на длинное нажатие.
    - Как применить функцию к CallBack.
    - !!! CallBack - Это к тому же отличная функция для того, чтобы вести логи.

--- hello_world/flutter_app/lib/main.dart
+++ hello_world/flutter_app/lib/main.dart
@@ -70,32 +70,45 @@ class _MyHomePageState extends State<MyHomePage> {
           shadowColor: Colors.blue,
           // Увеличение вертикального марджина.
           margin: EdgeInsets.all(10),
-          // Изменение вида Border.
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-          child: Column(
-            children: [
-              // Текстовый виджет.
-              Text(
-                "${events[index].name}",
-                style: TextStyle(fontSize: 30),
-              ),
-              // Виджет размещающий контент в строку.
-              Row(
-                // Выравнивание виджета по центру.
-                mainAxisAlignment: MainAxisAlignment.center,
-                // Заполнение строки.
-                children: [
-                  // Получить свойства объекта класса Event.
-                  Text(events[index].location),
-                  // Поставить между виджетами пустое пространство.
-                  SizedBox(
-                    width: 10,
-                  ),
-                  // Получить свойства объекта класса Event.
-                  Text(events[index].startDateTime.toString()),
-                ],
-              )
-            ],
+
+          // Создает список плитки.
+          child: ListTile(
+            // Заголовок
+            title: Text(
+              events[index].name,
+              // Изменение стиля заголовка (шрифт)
+              style: TextStyle(fontSize: 30),
+            ),
+            // Дополнительный контент - отображается ниже title.
+            subtitle: Text(
+                '${events[index].location} ${events[index].startDateTime.toString()}'),
+            // Виджет (иконка), отображается перед title.
+            leading: Icon(
+              Icons.local_activity,
+              size: 40,
+              color: Colors.black,
+            ),
+            // Виджет (иконка), отображается после title.
+            trailing: IconButton(
+              icon: Icon(Icons.edit),
+              // Повесил колбэк на виджет.
+              onPressed: null,
+            ),
+
+            // Повесить колбэк - на саму карточку, чтобы он срабатывал при нажатии.
+            // В консоль будет выводится информация (при помощи функции), когда пользователь коснется плитки.
+            // * Это можно использовать, когда обдумываешь как делать логирование действий пользователя.
+            onTap: () => print("${events[index].name} - tap!"),
+
+            // Колбэк на более долгое нажатие.
+            onLongPress: () => print("${events[index].name} - longPress!"),
+
+            // Disable на все нажатия.
+            // enabled: false,
+
+            // Это можно использовать, когда надо чтобы пользователь мог выделить объект
+            // *(коротким\длинным тапом).
+            selected: true,
           ),
         ),
       ),
